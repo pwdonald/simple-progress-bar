68ab7e4eab052a5c89282ed3e0c22b5d61067708
Donald Jones <donald@getplatypi.com>
2014-09-30 10:43:15 -0500

    Initial Commit

---
+++ simple-progres-bar/progress.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="_references.ts" />
+
+class ProgressBar {
+    private __visibleArray: Array<string> = new Array[10]();
+
+    constructor(label: string = 'Progress', format: string = '%label %bar %percent', private __output: NodeJS.WritableStream = process.stdout) {
+        this.__output.write('\r\n');
+    }
+
+    update(percent: number) {
+        
+    }
+
+}
+
+export = ProgressBar;
