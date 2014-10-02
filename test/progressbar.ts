210d5ae31ded2785cdbabb70c692c13c278acb6e
Donald Jones <donald@getplatypi.com>
2014-10-02 11:01:34 -0500

    Added test file.

---
+++ test/progressbar.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="../_references.ts" />
+import Progressbar = require('../progress');
+import events = require('events');
+
+var pb = new Progressbar('Test Value', ':label \t [ :bar ] :percent', process.stdout),
+    percentageComplete = 0,
+    totalPercentage = 100,
+    pubsub = new events.EventEmitter();
+
+var timer = setInterval(() => {
+    if (percentageComplete <= 90) {
+        percentageComplete += 10;
+        pubsub.emit('progress', percentageComplete);
+    } else {
+        pubsub.emit('done');
+    }
+}, 1000);
+
+pubsub.on('progress', (progressCount) => {
+    pb.update(progressCount, totalPercentage);
+});
+
+pubsub.on('done', () => {
+    process.stdout.write('\r\nDone!');
+    clearInterval(timer);
+});
